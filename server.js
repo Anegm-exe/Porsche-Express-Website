a0ee48f1ee310a90e1fb6fc054ed60f34277ddf4
coolAbdo <abdelrahman.egschool@gmail.com>
2024-05-01 19:05:54 +0300

    Made the navbar buttons redirect you to the page and not the html so for ex it says /about not /about.html

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'Porsche/Pages');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, 'Porsche/Pages/Home.html'));
 });
+app.get('/:page', (req, res) => {
+  const page = req.params.page;
+  if(page)
+    res.sendFile(path.join(__dirname, `Porsche/Pages/${page}.html`));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
