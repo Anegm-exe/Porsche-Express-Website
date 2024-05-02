bcc82fb8ec7d914341d4d0d6aecf83bde1367fa0
dersleepy <mostafa.mosalam04@gmail.com>
2024-05-02 19:57:09 +0300

    1. MondoDB connection 2. Authentication

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // Express App
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const secretKey = process.env.SECRET_KEY || '3kWn7$!l#X@3l7cF';
+
 
 const publicPath = path.join(__dirname,'Porsche/Pages');
 app.use(express.static(publicPath));
@@ -32,3 +35,35 @@ app.get('/:page', (req, res) => {
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// JWT verification middleware
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      res.redirect('/home');
+      next();
+  });
+}
+
+// MongoDB connection
+const { MongoClient } = require('mongodb');
+
+const mongoURI = 'mongodb+srv://porschteez:d7NzmIIfk7Nkj8fY@cluster0.aoy9mn2.mongodb.net/';
+
+const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+        // You can add more MongoDB operations here.
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err);
+    }
+}
+
+run().catch(console.dir);
