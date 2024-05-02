340907bd57efc76d8bafb5b5f284f6b78466f982
coolAbdo <abdelrahman.egschool@gmail.com>
2024-05-02 03:32:03 +0300

    icon now works and added few changes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+
+// Express App
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,16 +11,24 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'Porsche/Pages');
 app.use(express.static(publicPath));
 
-app.get('', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Porsche/Pages/Home.html'));
+// Page Loaders
+app.get('/', (req, res) => {
+  res.redirect('/Home')
 });
+
 app.get('/:page', (req, res) => {
   const page = req.params.page;
-  if(page)
-    res.sendFile(path.join(__dirname, `Porsche/Pages/${page}.html`));
+  const filePath = path.join(__dirname, `Porsche/Pages/${page}.html`);
+
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).send('Page not found');
+  }
 });
 
+// 
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
