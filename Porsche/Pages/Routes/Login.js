70b69a7faf0f20cb75cec8defa608ac26945d1ad
coolAbdo <abdelrahman.egschool@gmail.com>
2024-05-03 15:57:53 +0300

    LOGIN WORKS BABYYYYY!!!!!

--- Porsche/Pages/Routes/Login.js
+++ Porsche/Pages/Routes/Login.js
@@ -1,29 +1,31 @@
 const jwt = require('jsonwebtoken');
 const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcrypt');
+
 
 module.exports = async (req, res) => {
-    const { Email, Password } = req.body;
+    const { email, password } = req.body;
     const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     const db = client.db('porsche');
     const collection = db.collection('Customer');
-    console.log(Email + " " + Password);
-    const user = await collection.findOne({ email : Email });
+    const user = await collection.findOne({ email : email });
     if (!user) {
         collection = db.collection('Admin');
-        user = await collection.findOne({ email : Email });
+        user = await collection.findOne({ email : email });
         if (!user) return res.status(400).json({ message: 'User does not exist' });
     }
     
-    const isMatch = await bcrypt.compare(Password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         return res.status(403).send('Invalid password');
     }
 
-    delete user.Password;
+    delete user.password;
     const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "1h" });
+
     res.cookie("token", token, {
         httpOnly: true
     });
-    return res.redirect('/Home');
+    return res.status(200).redirect('/Home');
 };
