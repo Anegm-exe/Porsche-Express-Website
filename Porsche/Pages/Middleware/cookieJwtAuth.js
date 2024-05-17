d3eeeb3ce0b89b2cc6d08fa77c30f920cb0fe250
coolAbdo <abdelrahman.egschool@gmail.com>
2024-05-17 16:15:18 +0300

    cookie fixed

--- Porsche/Pages/Middleware/cookieJwtAuth.js
+++ Porsche/Pages/Middleware/cookieJwtAuth.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 exports.cookieJwtAuth = (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.body.cookies.token;
     try {
         const user = jwt.verify(token, process.env.SECRET_KEY);
         req.user = user;
         next();
     } catch (err) {
         res.clearCookie("token");
-        return res.redirect('/Signin');
+        return res.status(400).json({message:'Session ended please signin again'});
     }
 }
